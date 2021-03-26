fe88682fbec6193a57c0b7f273e9c849785f1908
Rohin Bhushan <rohinbhushan@gmail.com>
2021-03-26 19:08:30 -0400

    finish graphs 1 and 3

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ const MAX_WIDTH = Math.max(1080, window.innerWidth);
 const MAX_HEIGHT = 720;
 const margin = {top: 40, right: 100, bottom: 40, left: 135};
 
-let graph_1_width = (MAX_WIDTH / 2) - 10, graph_1_height = 250;
+let graph_1_width = (MAX_WIDTH / 2) - 10, graph_1_height = 300;
 let graph_2_width = (MAX_WIDTH / 2) - 10, graph_2_height = 275;
-let graph_3_width = MAX_WIDTH / 2, graph_3_height = 575;
+let graph_3_width = MAX_WIDTH / 2 - margin.right, graph_3_height = 575;
 
 
 let svg1 = d3.select("#graph1")
@@ -22,12 +22,14 @@ let svg3 = d3.select("#graph3")
     .attr('class', 'map');
 
 var tip = d3.tip()
-            .attr('class', 'd3-tip')
-            .offset([-10, 0])
-            .html(function(d) {
-              return "<strong>Country: </strong><span class='details'>" + d.properties.name + "<br></span>" + "<strong>Number of wins: </strong><span class='details'>" + d.numWins +"</span>";
-            });
+    .attr('class', 'd3-tip')
+    .offset([-10, 0])
+    .html(function (d) {
+        return "<strong>Country: </strong><span class='details'>" + d.properties.name + "<br></span>" + "<strong>Winning percentage: <span class='details'>" + Math.floor(d.winpct * 100) + "%</strong></span>";
+    });
 
+let cur_start_year = 2010;
+let cur_end_year = 2019;
 
 svg3.call(tip);
 var projection = d3.geoNaturalEarth()
@@ -38,6 +40,9 @@ var path = d3.geoPath().projection(projection);
 
 let x1 = d3.scaleLinear()
     .range([0, graph_1_width - margin.left - margin.right]);
+
+let color1 = d3.scaleOrdinal()
+    .range(d3.quantize(d3.interpolateHcl(d3.hsl('#a55eea'), d3.hsl('#a55eea').brighter(0.75)), 20));
 
 let y1 = d3.scaleBand()
     .range([0, graph_1_height - margin.top - margin.bottom])
@@ -58,8 +63,13 @@ let y_axis_text = svg1.append("text")
     .style("text-anchor", "middle");
 
 // Add chart title
-let title = svg1.append("text")
+let title1 = svg1.append("text")
     .attr("transform", `translate(${(graph_1_width - margin.left - margin.right) / 2}, ${-10})`)
+    .style("text-anchor", "middle")
+    .style("font-size", 15);
+
+let title3 = svg3.append("text")
+    .attr("transform", `translate(${(graph_3_width - margin.left - margin.right) / 2}, ${-10})`)
     .style("text-anchor", "middle")
     .style("font-size", 15);
 
@@ -73,26 +83,6 @@ slider.on("slideStop", function (range) {
     updateDashboard();
 });
 
-/**
- * Set the data source for bar and scatter plots between artist and song
- */
-function setData(attr) {
-    cur_attr = attr;
-    attr_input.placeholder = sentenceCase(attr);
-    updateDashboard();
-}
-
-/**
- * Updates cur attribute with new artist or song from user input
- */
-function setAttr() {
-    if (cur_attr === "artist") {
-        cur_artist = attr_input.value;
-    } else {
-        cur_song = attr_input.value;
-    }
-    updateDashboard();
-}
 
 /**
  * Updates dashboard scatterplot and barplot after change in date or cur_attr
@@ -102,52 +92,11 @@ function setAttr() {
 
 function updateDashboard() {
 
-    d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson").then(function (data) {
-        d3.csv("./data/winners.csv").then(function (winnersData) {
-            winnersForName = {};
-            winnersData.forEach(function(d) { winnersForName[d.country_name] = d.wins; });
-            data.features.forEach(function (d) {
-                console.log(d);
-                let name = d.properties.name === "USA" ? "United States" : d.properties.name;
-                let numWins = winnersForName[name];
-                d.numWins = numWins == null ? 0 : numWins;
-            });
-            svg3.append("g")
-                .attr("class", "countries")
-                .selectAll("path")
-                .data(data.features)
-                .enter().append("path")
-                .attr("d", path)
-                .style("fill", function (d) {
-                    return '#e3d47e'
-                })
-                .style('stroke', 'white')
-                .style('stroke-width', 1.5)
-                .style("opacity", 0.8)
-                // tooltips
-                .style("stroke", "white")
-                .style('stroke-width', 0.3)
-                .on('mouseover', function (d) {
-                    tip.show(d);
-
-                    d3.select(this)
-                        .style("opacity", 1)
-                        .style("stroke", "white")
-                        .style("stroke-width", 3);
-                })
-                .on('mouseout', function (d) {
-                    tip.hide(d);
-
-                    d3.select(this)
-                        .style("opacity", 0.8)
-                        .style("stroke", "white")
-                        .style("stroke-width", 0.3);
-                });
-        });
-    });
 
     d3.csv("./data/years.csv").then(function (data) {
         // TODO: Update the x1 axis domain with the max count of the provided data
+        console.log(data);
+        data = data.splice(cur_start_year - 2000, cur_end_year - 2000 + 1);
         console.log(data);
         console.log(d3.max(data, function (d) {
             return parseInt(d.count);
@@ -161,7 +110,7 @@ function updateDashboard() {
         y1.domain(data.map(function (d) {
             return d.year
         }));
-        //color.domain(data.map(function(d) { return d.attr }));
+        color1.domain(data.map(function(d) { return d.year }));
 
         // Render y1-axis label
         y1_axis_label.call(d3.axisLeft(y1).tickSize(0).tickPadding(10));
@@ -177,7 +126,7 @@ function updateDashboard() {
             .merge(bars)
             .transition()
             .duration(1000)
-            // .attr("fill", function(d) { return color(d.attr) })
+             .attr("fill", function(d) { return color1(d.year) })
             .attr("x", x1(0))
             .attr("y", function (d) {
                 return y1(d.year);
@@ -212,19 +161,11 @@ function updateDashboard() {
             });
         // Add y1-axis text and chart title
         y_axis_text.text("Year");
-        title.text("Number of football games played each year");
+        title1.text("Number of football games played each year");
         // Remove elements not in use if fewer groups in new dataset
         bars.exit().remove();
         counts.exit().remove();
     });
-}
-
-/**
- * Checks if a date falls within a provided year range
- */
-function validYear(start, end, cur) {
-    return (Date.parse(start) < Date.parse(cur)) &&
-        (Date.parse(cur) < Date.parse(end));
 }
 
 /**
@@ -235,25 +176,6 @@ function sentenceCase(word) {
 }
 
 /**
- * Abbreviates and shortens a given label by adding ellipses
- */
-function trimText(label) {
-    if (label.length > 20) {
-        return label.substring(0, 20) + "..."
-    }
-    return label;
-}
-
-/**
- * Finds all artists collaborating on a song by splitting on predefined text
- * and returns a list of all artists
- */
-function splitArtist(artist) {
-    let song_artists = artist.split(/(?:Featuring|&|,)/);
-    return song_artists.map(s => trimText(s.trim()));
-}
-
-/**
  * Returns a darker shade of a given color
  */
 function darkenColor(color, percentage) {
@@ -261,3 +183,65 @@ function darkenColor(color, percentage) {
 }
 
 updateDashboard();
+
+d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson").then(function (data) {
+        d3.csv("./data/win_pct.csv").then(function (winnersData) {
+            winnersForName = {};
+            title3.text("Winning Percentages by Nation");
+            winnersData.forEach(function (d) {
+                winnersForName[d.country_name] = d.win_pct;
+            });
+            data.features.forEach(function (d) {
+                let name = d.properties.name === "USA" ? "United States" : d.properties.name;
+                let winpct = winnersForName[name];
+                d.winpct = winpct == null ? 0 : winpct;
+            });
+            svg3.append("g")
+                .attr("class", "countries")
+                .selectAll("path")
+                .data(data.features)
+                .enter().append("path")
+                .attr("d", path)
+                .style("fill", function (d) {
+                    let name = d.properties.name === "USA" ? "United States" : d.properties.name;
+                    let winpct = winnersForName[name];
+                    if (winpct == null) {
+                        return '#efefef';
+                    }
+                    if (winpct > 0.58) {
+                        return '#eb3b5a'
+                    }
+                    let baseColor = d3.hsl('#a55eea').brighter(0.75);
+                    return darkenColor(baseColor, winpct * 2);
+
+                })
+                .style('stroke', 'white')
+                .style('stroke-width', 1.5)
+                .style("opacity", 0.8)
+                // tooltips
+                .style("stroke", "white")
+                .style('stroke-width', 0.3)
+                .on('mouseover', function (d) {
+                    let name = d.properties.name === "USA" ? "United States" : d.properties.name;
+                    let numWins = winnersForName[name];
+                    if (numWins == null) {
+                        return;
+                    }
+                    tip.show(d);
+
+                    d3.select(this)
+                        .style("opacity", 1)
+                        .style("stroke", "white")
+                        .style("stroke-width", 3);
+                })
+                .on('mouseout', function (d) {
+                    tip.hide(d);
+
+                    d3.select(this)
+                        .style("opacity", 0.8)
+                        .style("stroke", "white")
+                        .style("stroke-width", 0.3);
+                });
+
+        });
+    });
